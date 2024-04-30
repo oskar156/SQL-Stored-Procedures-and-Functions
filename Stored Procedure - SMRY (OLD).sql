364f5ab8fcd5710c6de890fdf891fbd487e69fab
Oscar Keshishyan <89118890+oskar156@users.noreply.github.com>
2024-04-30 15:42:55 -0700

    Update Stored Procedure - SMRY (OLD).sql

--- Stored Procedure - SMRY (OLD).sql
+++ Stored Procedure - SMRY (OLD).sql
@@ -6,12 +6,9 @@
 	-all following columns are bracketed and preceded by a comma ,[]
 
 -1st parameter, tablename is required
--2nd parameter, col is optional
-	-if left blank it will summarize the table
-	-if filled in, then it will summarize the column instead
--3rd parameter, dbname is optional 
+-2nd parameter, dbname is optional 
 	-if left blank it will use the DB currently in use
--4th parameter, count is optional 
+-3rd parameter, count is optional 
 	-if left blank it will default to showing the count
 	-if passed any value other than 'count', then it will not show the count
 	-SMRY is all about convenience and speed, some tables are very large and will take a long time to get their count, that's why it's optional
@@ -19,7 +16,7 @@
 
 USE [TEMP_OK]
 GO
-/****** Object:  StoredProcedure [dbo].[SMRY]    Script Date: 11/21/2023 2:21:41 PM ******/
+/****** Object:  StoredProcedure [dbo].[SMRY_OLD]    Script Date: 4/30/2024 3:42:19 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -29,10 +26,10 @@ GO
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-ALTER PROCEDURE [dbo].[SMRY]
+ALTER PROCEDURE [dbo].[SMRY_OLD]
 	-- Add the parameters for the stored procedure here
 	@TABLENAME VARCHAR(255)
-	,@COL VARCHAR(255)=''
+	--,@COL VARCHAR(255)=''
 	,@DBNAME VARCHAR(255)=''
 	,@COUNT VARCHAR(255)='count'
 AS
@@ -51,37 +48,24 @@ BEGIN
 	--SELECT <@Param1, sysname, @p1>, <@Param2, sysname, @p2>
     DECLARE @Sql NVARCHAR(4000);
 
-	IF @COL='' BEGIN-- THEN SUMMARIZE THE TABLE 
-		--Get count
-		IF lower(@COUNT) = 'count' BEGIN
-			SET @Sql = N' SELECT COUNT(*) as [COUNT]  FROM [' + @DBNAME + ']..[' + @TABLENAME + '] '
-			EXECUTE sp_executesql @Sql
-		END
+    --Get count
+     IF lower(@COUNT) = 'count' BEGIN
+          SET @Sql = N' SELECT COUNT(*) as [COUNT]  FROM [' + @DBNAME + ']..[' + @TABLENAME + '] '
+          EXECUTE sp_executesql @Sql
+     END
 
-		--Get top 10
-		SET @Sql = N' SELECT TOP 10 * FROM [' + @DBNAME + ']..[' + @TABLENAME + '] '
-		EXECUTE sp_executesql @Sql
+    --Get top 10
+	SET @Sql = N' SELECT TOP 10 * FROM [' + @DBNAME + ']..[' + @TABLENAME + '] '
+	EXECUTE sp_executesql @Sql
 
-		--Get all Column Names in a Table and surround them with ,[] (original order)
-		SET @Sql = N' SELECT CONCAT(''['',COLUMN_NAME,'']'') as [COLUMN_NAMES]'
-		SET @Sql = @Sql + ' FROM [' + @DBNAME + '].information_schema.columns '
-		SET @Sql = @Sql + ' WHERE table_name = ''' + @TABLENAME + ''' '
-		EXECUTE sp_executesql @Sql
-	END
+    --Get all Column Names in a Table and surround them with ,[] (original order)
+	SET @Sql = N' SELECT CONCAT(CASE WHEN ROW_NUMBER() OVER(order by (SELECT NULL)) <> 1 THEN '',''END,''['',COLUMN_NAME,'']'') as [COLUMN_NAMES]'
+	SET @Sql = @Sql + ' FROM [' + @DBNAME + '].information_schema.columns '
+	SET @Sql = @Sql + ' WHERE table_name = ''' + @TABLENAME + ''' '
+	EXECUTE sp_executesql @Sql
 
-	IF @COL<>'' BEGIN-- THEN SUMMARIZE THE COLUMN 
 
-		SET @Sql = N' SELECT COUNT([' + @COL + ']) as [COUNT] '
-		SET @Sql = @Sql + ' , COUNT(CASE WHEN [' + @COL + '] IS NOT NULL AND [' + @COL + ']<>'''' THEN 1 END) AS [NOT BLANK COUNT] '
-		SET @Sql = @Sql + ' , COUNT(DISTINCT [' + @COL + '] ) AS [DISTINCT COUNT] '
-		SET @Sql = @Sql + ' FROM [' + @DBNAME + ']..[' + @TABLENAME + '] '
-		EXECUTE sp_executesql @Sql
-		
-		SET @Sql = N' SELECT [' + @COL + '], COUNT(*) AS [COUNT] '
-		SET @Sql = @Sql + ' FROM [' + @DBNAME + ']..[' + @TABLENAME + '] '
-		SET @Sql = @Sql + ' GROUP BY [' + @COL + '] '
-		SET @Sql = @Sql + ' ORDER BY    COUNT(*) DESC ,  [' + @COL + '] ASC '
-		EXECUTE sp_executesql @Sql
-	END
+
+
 END
 
